54936cd60bee73e23833fee11c09941446f406c9
starving-array <gowork.archis@gmail.com>
2022-12-24 03:06:18 +0530

    Day 1

---
+++ Day1/brand_name_generator.py
@@ -0,0 +1,15 @@
+#1. Create a greeting for your program.
+print("Hi, welcome")
+
+#2. Ask the user for the city that they grew up in.
+name = input("Please enter your name? ")
+
+#3. Ask the user for the name of a pet.
+pet = input("Please enter name of your pet? ")
+
+#4. Combine the name of their city and pet and show them their band name.
+band = name +" "+ pet
+
+#5. Make sure the input cursor shows on a new line:
+print("Your band name "+ band)
+print("")
